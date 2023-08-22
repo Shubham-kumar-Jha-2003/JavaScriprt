337bd4794488158fadfb3e8a2ac323222dc9f67d
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-22 14:04:43 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+// Comparisons in javaScriot
+// node 01_basics/04_comparisons.js
+
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+
+// Strict comparisons like => (===), (!==)
+console.log("2" == 2)
+console.log("2" === 2);
