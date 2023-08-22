c74c5353b10a57bcb955f981b6fd2e4e0e0e9701
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-22 15:55:52 +0000

    About Stack Heap and Reference

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,12 +12,12 @@ const bigNumber = 333748374973n;
 const outsideTemp = null;
 
 let userEmail;
-console.log(userEmail);
+// console.log(userEmail);
 
 let id = Symbol('123');
 
 let anotherId = Symbol('123');
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // Reference (Non Primitives)
 
@@ -35,7 +35,35 @@ const myFunction = function() {
     console.log("Hello Shubham");
 }
 
-myFunction();
+// myFunction();
 
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitives), heap (Non-primitives / Reference )
+
+let myYotubename = "ShubhamKumarjha";
+
+let anotherName = myYotubename;
+
+// console.log(myYotubename);
+// console.log(anotherName);
+
+
+anotherName = "ChaiaurCode";
+
+console.log(myYotubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@wwwgoogle.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "shubhamkumarjha855@gmail.com";
+console.log(userOne)
+console.log(userTwo);
