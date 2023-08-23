836f7d77e8639300a41ff366bd6afd4342c46b61
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-23 15:30:39 +0000

    About Math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(  balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1384.32736;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(8.9));
+// console.log(Math.min(4,5,9,3,2));
+// console.log(Math.max(4,5,9,3,2));
+
+
+// For genrate Random number 
+// console.log(Math.random());
+// console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10;
+
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
