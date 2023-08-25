7bf6b4fc5618cadb8a9a893f4f55c74dc3f0163c
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-25 13:14:40 +0000

    About scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,66 @@
+
+// var c = 10;
+let a = 300
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    // var c = 30
+    
+}
+
+
+// console.log(a)
+
+function one() {
+
+    const username = "Shubham";
+
+    function two() {
+
+        const website = "github.com";
+
+        console.log(username);
+    }
+
+    
+    // console.log(website);
+    two();
+}
+
+// one();
+
+if(true) {
+
+    const username = "Shubham";
+
+    if(username === "Shubham") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+
+
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++ Intersting ++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5))
+
+function addOne(num) {
+    
+    return num + 1;
+}
+
+
+addTwo(5)
+const addTwo = function(num) {
+    
+    return num + 2;
+}
+
+
+
