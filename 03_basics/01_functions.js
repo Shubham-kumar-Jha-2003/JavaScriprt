9d452735f544350f84d3a0466290c3a8ce2aecc3
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-25 12:22:21 +0000

    functions within object and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,50 @@ const userLoggInMessage = function logginUserMessage(username = "Afsha") {
     return `${username} Just logged in Instagram`;
 }
 
-console.log(userLoggInMessage())
+// console.log(userLoggInMessage())
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 300, 580, 800, 2000));
+
+
+const user = {
+    username: "Shubham",
+    Price: 199
+}
+
+function handleObject(anyobject) {
+
+    console.log(`username is ${anyobject.username} and price is ${anyobject.Price}`);
+}
+
+// handleObject(user);
+
+handleObject({
+    username: "Afsha",
+    Price: 399
+})
+
+const myNewArray = [200, 100, 300, 600];
+
+function returnSecondValue(getArray) {
+
+    return getArray[2];
+
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 100, 500, 504]));
+
+// Yeh mera dimag ha
+
+// const myNewArray = [200, 300, 100, 600];
+
+// function array (myNewArray) {
+//     console.log(`${myNewArray}`)
+// }
+
+
+// array([200, 300, 400])
