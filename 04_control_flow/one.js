0c350849f97d420d528cfddaf826effc0466052b
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-26 11:31:17 +0000

    Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,70 @@
+// if
+
+// if(conditon) {
+//     // code execution if condition is true
+// }
+
+const isUserLoggedIn = true;
+
+const tempreature = 41;
+
+// if (tempreature === 41) {
+//     console.log("tempreature is less then 50")
+// } else {
+
+//     console.log("Tempreture is greater then 50 ");
+// }
+
+// console.log('Hello This is not conditonals statements');
+
+
+// if(2 == "2") {
+//     console.log('True');
+// }
+
+// <, >, <=, >=, != ==, ===
+
+// const score = 200
+
+// if(score > 100) {
+
+//     const power = "fly"
+
+//     console.log(`User Power : ${power}`)
+// }
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("less then 500");
+// } else if (balance < 750) {
+//     console.log("less then 500");
+// } else if(balance < 900) {
+//     console.log("less then 500");
+    
+// } else {
+//     console.log("less then 1200");
+// }
+
+// if(balance > 500) console.log("test"), console.log("test2");  // This will nor prefereable code
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (isUserLoggedIn && debitCard) {
+
+    console.log("Allow to buy course")
+    
+}
+
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in")
+}
+
+
+
+
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,19 @@
+// truththy and falsy 
+
+const userEmail = "s@shubham,.ai"
+
+if(userEmail) {
+
+    console.log("Got user email  ")
+} else {
+    console.log("Don't have user email ")
+}
+
+
+// falsy value
+
+// false , 0, -0, "", NaN, BIGINT 0n, null, undefined 
+
+// truthy value
+
+// "0", 
