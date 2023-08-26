cb215fd02b4c4b6f854c9f04f70d2559d493fbac
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-26 13:56:49 +0000

    some change in truthy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,6 +1,6 @@
 // truththy and falsy 
 
-const userEmail = "s@shubham,.ai"
+const userEmail = []
 
 if(userEmail) {
 
@@ -16,4 +16,35 @@ if(userEmail) {
 
 // truthy value
 
-// "0", 
+// "0", " " "false", [] {} function(){}
+
+if(userEmail.length === 0) {
+    console.log('Array is empty');
+}
+
+const emptyObject = {}
+
+if(Object.keys(emptyObject).length === 0) {
+    console.log("Object is empty")
+}
+
+// Nullish Coalescing Operator(??) null undefined
+
+let val1 = 5 ?? 10;
+
+// val1 = null ?? 3;
+// val1 =  undefined ?? 18;
+// val1 = null ?? 10 ?? 20
+
+// Ternary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log("less then 80") : console.log("more then 80")
+
+
+console.log(val1)
+
+
