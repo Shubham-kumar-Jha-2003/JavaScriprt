a88bfa9df2b1a301b10194b5098e0cb44b8bcf5c
Shubham Kumar Jha <135526109+Shubham-kumar-Jha-2003@users.noreply.github.com>
2023-08-27 03:56:30 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+// while loop
+
+let index = 0;
+
+let myArray = ["Shubham", "Afsha", "Sumit"];
+
+while (index < myArray.length) {
+
+    console.log(`value of index is ${myArray[index]}`);
+    index++;
+    
+}
